45dfeeb7bc0bd5a6a208d6a0e1cd375b9438aed6
Zeinab Gamal <zeinab.gamal512@gmail.com>
2019-11-14 16:19:09 +0200

    Updates

---
+++ precache-manifest.498a6e9c75fb14198b2bec2c51620319.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68aac0ee6752938b63218b2d1972547d",
+    "url": "/material-ui-porfolio/index.html"
+  },
+  {
+    "revision": "546a10d02ccc02921e8a",
+    "url": "/material-ui-porfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e8d282262b52b3f812bb",
+    "url": "/material-ui-porfolio/static/js/2.e56066f3.chunk.js"
+  },
+  {
+    "revision": "546a10d02ccc02921e8a",
+    "url": "/material-ui-porfolio/static/js/main.1eb57763.chunk.js"
+  },
+  {
+    "revision": "d955ff3e5e45a30aa03e",
+    "url": "/material-ui-porfolio/static/js/runtime-main.6a26b766.js"
+  }
+]);
